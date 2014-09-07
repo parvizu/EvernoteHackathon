a79f88cb7a99cb5f1dfef73f5e1fa1412d9468aa
Pablo <parvizu@ischool.berkeley.edu>
2014-09-06 18:26:57 -0700

    updates

--- static/chrome/collector.js
+++ static/chrome/collector.js
@@ -9,7 +9,7 @@ var log = {};
 
 $(document).ready(function() {
 	
-	$('body').prepend("<div style='position: absolute;left:200px;z-index:100;'>Enter Group Session ID: <input id='session_id' type='text' width='200'><input id='btn_session_start' type='button' value='Start'><input id='btn_session_end' type='button' value='Stop'></div>");
+	$('body').prepend("<div style='position: absolute;left:200px;z-index:100;'>Enter Group Session ID: <input id='session_id' type='text' width='200'><input id='btn_session_start' type='button' value='Start'><input id='btn_session_end' type='button' value='Stop' style='display:none;'></div>");
 
 	$('#btn_session_start').click(function() {
 		$.post('http://127.0.0.1:5000/enter_group', { groupid: $('#session_id').val() }, function( data ) {
